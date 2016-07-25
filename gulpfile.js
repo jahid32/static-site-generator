2262f72d06a5f76a610f8bca18afb47445ac912f
Mostafizur Rahman <Mostafizur Rahman>
2016-07-26 02:19:28 +0600

    Re structure file and re configure gulp

--- gulpfile.js
+++ gulpfile.js
@@ -8,85 +8,74 @@ var gulp  = require('gulp'),
     uglify = require('gulp-uglify'),
     sass  = require('gulp-sass'),
     jade  = require('gulp-jade'),
-    connect = require('gulp-connect'),
     open = require("gulp-open"),
     plumber= require('gulp-plumber'),
-    browserSync = require('browser-sync'),
-    reload = browserSync.reload;
+    bs = require('browser-sync').create();
 
+var paths = {
+  scss: 'assets/scss/*.scss',
+  jade: 'assets/jade/*.jade',
+  js: 'assets/js/*.js'
+} ;
 
 /**
  * [Check javascript error and compress script]
  */
-gulp.task('scripts',function(){
-  gulp.src('js/*.js')
+gulp.task('js',function(){
+  gulp.src(paths.js)
     .pipe(plumber())
     .pipe(uglify())
     .pipe(gulp.dest('build/js'))
-    .pipe(connect.reload());
+    .pipe(bs.reload({stream: true}));
 });
 
 /**
  * Compile sass into css and minify.
  */
 gulp.task('style',function(){
-  gulp.src('scss/*.scss')
+  gulp.src(paths.scss)
     .pipe(plumber())
     .pipe(sass({
       includePaths: require('node-bourbon').includePaths,
       style: 'compressed'
     }))
     .pipe(gulp.dest('build/css/'))
-    .pipe(connect.reload());
+    .pipe(bs.reload({stream: true}));
 });
 
 /**
  * Compile jade to html
  */
-gulp.task('jade',function(){
-  gulp.src("jade/*.jade")
+gulp.task('html',function(){
+  gulp.src(paths.jade)
     .pipe(plumber())
     .pipe(jade())
     .pipe(gulp.dest('build/'))
-    .pipe(connect.reload());
+    .pipe(bs.reload({stream: true}));
 });
 
-gulp.task('connect', function() {
-  connect.server({
-    root: 'build',
-    livereload: true
-  });
-});
-// This will use the default applications
 
-//gulp.task("serve", function(){
-//  gulp.src("build/index.html")
-//  .pipe(open("<%file.path%>",{app:"chrome", url: "http://localhost:8080"}));
-//  gulp.watch('js/**/*.js', ['scripts']);
-//  gulp.watch('scss/**/*.scss', ['style']);
-//  gulp.watch('jade/**/*.jade', ['jade']);
-//});
-// Watch Files For Changes & Reload
-gulp.task('serve', function () {
-  browserSync({
-    notify: false,
-    server: {
-      baseDir: ['.tmp', 'build']
-    }
-  });
-  gulp.watch('jade/**/*.jade', ['jade']);
-  gulp.watch(['build/**/*.html'], reload);
-  gulp.watch('scss/**/*.scss', ['style']);
-  gulp.watch(['build/css/*.css'], reload);
-  gulp.watch('js/**/*.js', ['scripts']);
+// Watch Files For Changes & Reload withnode server
+gulp.task('server', ['html', 'style', 'js'], function () {
+  bs.init({
+        server: {
+            baseDir: "./build"
+        },
+            port: 9000
+        
+        //  proxy: "yourlocal.dev"
+    });
+  // gulp.watch(paths.jade, ['html']);
+  // gulp.watch(paths.scss, ['style']);
+  // gulp.watch(paths.js, ['js']);
 });
 // Watch task
 // It's watches java script
 gulp.task('watch',function(){
-  gulp.watch('js/**/*.js', ['scripts']);
-  gulp.watch('scss/**/*.scss', ['style']);
-  gulp.watch('jade/**/*.jade', ['jade']);
+  gulp.watch(paths.jade, ['html']);
+  gulp.watch(paths.scss, ['style']);
+  gulp.watch(paths.js, ['js']);
 });
 
-gulp.task('default', ['scripts', 'style',  ,'serve']);
+gulp.task('default', ['js', 'style', 'html' ,'serve']);
 
